e21564cb58bab9880b4bfcdccb1ffe11f7556b5a
HerrDmitry <HerrDmitry@gmail.com>
2017-09-16 22:11:47 -0400

    updates

---
+++ DataMover/Interfaces/IDataRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataMover.Interfaces
+{
+    public interface ISourceRow
+    {
+        string[] Columns { get; }
+        string Source { get; }
+        bool HasError { get; }
+        string Error { get; }
+    }
+}
