6cb82fbd9764e9cd3574213d2793d09ca8ff1a0a
HerrDmitry <HerrDmitry@gmail.com>
2017-09-17 21:41:22 -0400

    updates

---
+++ Interfaces/ISourceRow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface ISourceRow
+    {
+        IEnumerable<string>[] Columns { get; }
+        string Source { get; }
+        bool HasError { get; }
+        string Error { get; }
+        long RowNumber { get; }
+        long ParsedLineNumber { get; }
+        long RawLineNumber { get; }
+    }
+}
---
+++ Interfaces/IStreamReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Interfaces
+{
+    public interface IStreamReader
+    {
+        IEnumerable<ISourceRow> Read(Stream stream);
+    }
+}
