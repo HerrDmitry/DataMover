e765e66a2be06d86bf6ed9d3b0f4bfa887565ae1
Dmitry Balobanov <dmitry.balobanov@thinklp.com>
2017-09-19 13:46:17 -0400

    project rename

---
+++ FileReader/Reader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Interfaces;
+
+namespace CsvReader
+{
+    public class Reader:IStreamReader
+    {
+        public IEnumerable<IRawLine> Read(Stream stream, IConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
